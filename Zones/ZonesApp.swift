b38c28e074afe819a491f82428bdae5196f9780b
Helia <helia@desap.co.uk>
2023-10-30 09:21:58 +0330

    Initial Commit

---
+++ Zones/ZonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZonesApp.swift
+//  Zones
+//
+//  Created by Helia Fathi on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ZonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
